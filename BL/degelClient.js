f51aa3f194cbfd38269fe0f6001cc5a8ccb3cabe
Philippe Girard <philippe.girard5@gmail.com>
2018-06-25 22:55:15 -0400

    notifications settings

--- BL/degelClient.js
+++ BL/degelClient.js
@@ -3,25 +3,19 @@ import { BASE_URL } from '../constants/endpoints';
 
 class DegelClient {
 
-  constructor() {
-    this._accessToken = null;
-    this._refreshToken = null;
-  }
-
-  async init() {
-    this._accessToken = await AsyncStorage.getItem('access_token');
-    this._refreshToken = await AsyncStorage.getItem('refresh_token');
-  }
-
   // todo : parametrize body and method
-  async authFetch(url, method = 'POST') {
+  static async authorizedFetch(url, method = 'POST', body = null) {
+    _accessToken = await AsyncStorage.getItem('access_token');
+
     try {
       let response = await fetch(url, {
-        method: 'GET',
+        method: method,
         headers: {
           Accept: '*/*',
-          Authorization: 'Bearer ' + this._accessToken
-        }
+          Authorization: 'Bearer ' + _accessToken,
+          'Content-Type': 'application/json'
+        },
+        body: body
       });
 
       return response.json();
@@ -33,8 +27,8 @@ class DegelClient {
     return null;
   }
 
-  async getCurrentUser() {
-    currentUser = await this.authFetch(BASE_URL + '/api/user/current', 'GET');
+  static async saveCurrentUser() {
+    currentUser = await this.authorizedFetch(BASE_URL + '/api/user/current', 'GET');
     try {
       await AsyncStorage.setItem('cip', currentUser.cip);
       await AsyncStorage.setItem('id', currentUser.id);
@@ -43,6 +37,43 @@ class DegelClient {
       console.log("error : " + currentUser);
     }
     
+  }
+
+  static async getSettingsStatus() {
+    _id = await AsyncStorage.getItem('id');
+    settingsStateResponse = await this.authorizedFetch(BASE_URL + '/api/user/' + _id + '/settings', 'GET');
+    
+    settingsState = {
+      notification: false
+    }
+
+    try {
+      settingsState.notification = settingsStateResponse.notifications.mobile;
+    }
+    catch(error) {
+      console.log("something went wrong : ");
+      console.log(settingsStateResponse);
+    }
+
+    return settingsState;
+  }
+
+  static async setSettingsStatus(notification = false) {
+    _id = await AsyncStorage.getItem('id');
+
+    settingsState = {
+      "notifications": {
+        "mobile": notification
+      }
+    }
+
+    response = await this.authorizedFetch(
+      BASE_URL + '/api/user/' + _id + '/settings', 
+      'POST', 
+      JSON.stringify(settingsState)
+    );
+
+    console.log(response);
   }
 }
 
